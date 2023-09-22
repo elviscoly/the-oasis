83902a26c1bd8e5efa0d61230b0fcee4b5839de5
elviscoly <elviscoly2017@gmail.com>
2023-09-22 08:09:59 +0100

    Configure environment variable

--- src/services/superbase.js
+++ src/services/superbase.js
@@ -2,7 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabaseUrl = 'https://fxowisoytimkyfmyngzz.supabase.co'
-const supabaseKey = ""
+
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
